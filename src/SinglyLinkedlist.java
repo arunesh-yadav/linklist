8c665c6059ebba25ac644350f22812bf23c3f34a
arunesh-yadav <arunesh.kumar_ccv17@gla.ac.in>
2019-02-26 15:15:05 +0530

    execute

--- src/SinglyLinkedlist.java
+++ src/SinglyLinkedlist.java
@@ -18,5 +18,70 @@ public class SinglyLinkedlist {
         public void setNext(Node next) {
             this.next = next;
         }
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+    private Node head = null;
+    private int size=0;
+
+    public static void main(String[] args) {
+        SinglyLinkedlist linkedlist=new SinglyLinkedlist();
+        System.out.println(linkedlist);
+        for (int i = 0; i < 5; i++) {
+            linkedlist.insertHead(i+1);
+        }
+        linkedlist.insertHead(42);
+        linkedlist.insertHead(66);
+
+        System.out.println(linkedlist);
+
+    }
+
+    @Override
+    public String toString() {
+       StringBuilder response = new StringBuilder();
+       response.append("[");
+       Node temp = this.head;
+       while(temp != null)
+        {
+            response.append(temp.getData());
+            if (temp.next != null){
+                response.append(" ==> ");
+            }
+            temp = temp.next;
+        }
+        response.append("]");
+
+       return response.toString();
+    }
+    private void insertHead(int data){
+        Node newNode =  new Node(data ,this.head);
+        this.head = newNode;
+        size++;
+    }
+    private void insertAfter(int data,Node node){
+        Node newNode = new Node(data, node.next);
+        node.next=newNode;
+        size++;
+    }
+    public void insert (int data){
+        if (head==null){
+            insertHead(data);
+        }
+        else
+        {
+            Node temp = this.head;
+            while(temp.next!=null){
+                temp=temp.next;
+            }
+            insertAfter(data,temp);
+        }
     }
 }
